4b0ce57293c6436c2a6797bcf3093d0aa89dc717
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-23 18:52:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,30 +7,26 @@ def nyc_pigeon_organizer(pigeon_data)
   occups = {}
   x = {}
   
-  pigeon_data.each do |first_key,second_key|
-    second_key.each do |s_key, third_key|
-      x[s_key] = []
-    end
-  end
-  
-  z = {}
-  z[:hi] << "hi"
-  z[:hello] << "hello"
-  
   
   pigeon_data.each do |first_key,second_key|
     second_key.each do |s_key, third_key|
       third_key.each do |t_key|
-        new_hash[t_key] = {}
+        if new_hash[t_key] == nil
+          new_hash[t_key] = {}
+        else
+          if new_hash[t_key][s_key] ==nil
+            new_hash[t_key][s_key] = {}  
+          else
+            
+          end
+        end
       end
     end
   end
   
 
   
-  puts x
-  puts keys
-  puts z
+  puts new_hash
   return new_hash
   
   end
