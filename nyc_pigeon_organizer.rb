18eaf19c30cfa8d904b739e4c2035f9c010d4ab9
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-23 19:02:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
-def nyc_pigeon_organizer(data)
+
   # write your code here!
-end
+  
+def nyc_pigeon_organizer(pigeon_data)
+  new_hash = {}
+  
+  
+  
+  pigeon_data.each do |first_key,second_key|
+    second_key.each do |s_key, third_key|
+       string_s = s_key.to_s
+      third_key.each do |t_key|
+          if new_hash[t_key] == nil
+          new_hash[t_key] = {}
+          end
+          if new_hash[t_key][first_key] ==nil
+            new_hash[t_key][first_key] = {}  
+          end
+          if new_hash[t_key][first_key][string_s] == nil
+             new_hash[t_key][first_key][string_s] = {}
+          end
+        
+      end
+    end
+  end
+  
+
+  
+  puts new_hash
+  return new_hash
+  
+  end
